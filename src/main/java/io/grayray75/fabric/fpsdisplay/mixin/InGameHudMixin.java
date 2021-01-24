dfb7e7bc1816cd841bf6e2079b9f75828194a3b0
Grayray <69988482+Grayray75@users.noreply.github.com>
2021-01-24 18:37:08 +0100

    Backport to Minecraft 1.8.9

--- src/main/java/io/grayray75/fabric/fpsdisplay/mixin/InGameHudMixin.java
+++ src/main/java/io/grayray75/fabric/fpsdisplay/mixin/InGameHudMixin.java
@@ -2,7 +2,6 @@ package io.grayray75.fabric.fpsdisplay.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,17 +11,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class InGameHudMixin {
 
     @Inject(at = @At("TAIL"), method = "render")
-    public void render(MatrixStack matrixStack, float tickDelta, CallbackInfo info) {
+    public void render(float tickDelta, CallbackInfo info) {
         MinecraftClient client = MinecraftClient.getInstance();
 
         if (!client.options.debugEnabled) {
 
             String displayString = ((MinecraftClientMixin) client).getCurrentFPS() + " FPS";
-            float textPosX = 2;
-            float textPosY = 2;
+            int textPosX = 2;
+            int textPosY = 2;
             int textColor = 0xDDEEEEEE;
 
-            client.textRenderer.draw(matrixStack, displayString, textPosX, textPosY, textColor);
+            client.textRenderer.draw(displayString, textPosX, textPosY, textColor);
         }
     }
 }

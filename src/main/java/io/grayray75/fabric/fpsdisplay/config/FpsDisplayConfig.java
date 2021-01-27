6c712d12daeeecbe80f2ffe9e3d6a3432e208adb
Grayray <69988482+Grayray75@users.noreply.github.com>
2021-01-27 11:52:24 +0100

    Backport changes from v1.2.0

---
+++ src/main/java/io/grayray75/fabric/fpsdisplay/config/FpsDisplayConfig.java
@@ -0,0 +1,19 @@
+package io.grayray75.fabric.fpsdisplay.config;
+
+import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
+import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
+import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry;
+
+@Config(name = "fpsdisplay")
+public class FpsDisplayConfig implements ConfigData {
+
+    public boolean enabled = true;
+
+    public boolean drawWithShadows = false;
+
+    @ConfigEntry.ColorPicker
+    public int textColor = 0xEEEEEE;
+
+    @ConfigEntry.BoundedDiscrete(min = 0, max = 255)
+    public int textAlpha = 230;
+}
--- src/main/java/io/grayray75/fabric/fpsdisplay/mixin/InGameHudMixin.java
+++ src/main/java/io/grayray75/fabric/fpsdisplay/mixin/InGameHudMixin.java
@@ -7,21 +7,29 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import io.grayray75.fabric.fpsdisplay.config.FpsDisplayConfig;
+import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
 
     @Inject(at = @At("TAIL"), method = "render")
     public void render(float tickDelta, CallbackInfo info) {
         MinecraftClient client = MinecraftClient.getInstance();
+        FpsDisplayConfig config = AutoConfig.getConfigHolder(FpsDisplayConfig.class).getConfig();
 
-        if (!client.options.debugEnabled) {
+        if (!client.options.debugEnabled && config.enabled && config.textAlpha > 3) {
 
             String displayString = ((MinecraftClientMixin) client).getCurrentFPS() + " FPS";
             float textPosX = 2;
             float textPosY = 2;
-            int textColor = 0xDDEEEEEE;
+            int textColor = ((config.textAlpha & 0xFF) << 24) | config.textColor;
 
-            client.textRenderer.draw(displayString, textPosX, textPosY, textColor);
+            if (config.drawWithShadows) {
+                client.textRenderer.drawWithShadow(displayString, textPosX, textPosY, textColor);
+            } else {
+                client.textRenderer.draw(displayString, textPosX, textPosY, textColor);
+            }
         }
     }
 }

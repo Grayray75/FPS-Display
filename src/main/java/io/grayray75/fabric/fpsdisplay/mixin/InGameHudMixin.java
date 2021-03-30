1d2472661cddb4824bea40c1226390293129a32f
Kevin Köllmann <mail@kevinkoellmann.de>
2021-03-30 19:33:06 +0200

    Made the overlay positioning respect the current GUI scale

--- src/main/java/io/grayray75/fabric/fpsdisplay/mixin/InGameHudMixin.java
+++ src/main/java/io/grayray75/fabric/fpsdisplay/mixin/InGameHudMixin.java
@@ -22,8 +22,9 @@ public class InGameHudMixin {
         if (!client.options.debugEnabled && config.enabled && config.textAlpha > 3 && FpsDisplayMod.SHOW_FPS_OVERLAY) {
 
             String displayString = ((MinecraftClientMixin) client).getCurrentFPS() + " FPS";
-            float textPosX = config.offsetLeft;
-            float textPosY = config.offsetTop;
+            int guiScale = client.options.guiScale;
+            float textPosX = config.offsetLeft / guiScale;
+            float textPosY = config.offsetTop / guiScale;
             int textColor = ((config.textAlpha & 0xFF) << 24) | config.textColor;
 
             if (config.drawWithShadows) {

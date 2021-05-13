00b26a8e2e1a8097c94590aed671bcbfb667c3ae
Grayray <69988482+Grayray75@users.noreply.github.com>
2021-05-13 10:38:46 +0200

    Refactor code

--- src/main/java/io/grayray75/fabric/fpsdisplay/FpsDisplayMod.java
+++ src/main/java/io/grayray75/fabric/fpsdisplay/FpsDisplayMod.java
@@ -1,19 +1,17 @@
 package io.grayray75.fabric.fpsdisplay;
 
+import io.grayray75.fabric.fpsdisplay.config.FpsDisplayConfig;
+import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.Environment;
 import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-
 import org.apache.logging.log4j.LogManager;
 import org.lwjgl.glfw.GLFW;
-
-import io.grayray75.fabric.fpsdisplay.config.FpsDisplayConfig;
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
-import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
 
 @Environment(EnvType.CLIENT)
 public class FpsDisplayMod implements ClientModInitializer {
--- src/main/java/io/grayray75/fabric/fpsdisplay/mixin/MinecraftClientMixin.java
+++ src/main/java/io/grayray75/fabric/fpsdisplay/mixin/MinecraftClientMixin.java
@@ -6,6 +6,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(MinecraftClient.class)
 public interface MinecraftClientMixin {
-	@Accessor("currentFps")
-	public abstract int getCurrentFPS();
+    @Accessor("currentFps")
+    int getCurrentFPS();
 }

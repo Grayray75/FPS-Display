dccf49d2eeb77377015f2b359cd8f41657718ef6
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2023-07-18 13:33:46 +0200

    Port to Minecraft 1.18.2

--- src/main/java/io/grayray75/mods/fpsdisplay/gui/ClothOptionScreen.java
+++ src/main/java/io/grayray75/mods/fpsdisplay/gui/ClothOptionScreen.java
@@ -7,6 +7,7 @@ import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 
 // https://shedaniel.gitbook.io/cloth-config/
 
@@ -14,7 +15,7 @@ public class ClothOptionScreen {
     public static Screen generateScreen(Screen parent) {
         ConfigBuilder builder = ConfigBuilder.create();
         builder.setParentScreen(parent);
-        builder.setTitle(Text.translatable("text.fpsdisplay.options.title"));
+        builder.setTitle(new TranslatableText("text.fpsdisplay.options.title"));
         builder.setSavingRunnable(() -> {
             ConfigManager.saveConfig();
         });
@@ -25,43 +26,43 @@ public class ClothOptionScreen {
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
         ConfigCategory general = builder.getOrCreateCategory(Text.of("General"));
-        general.addEntry(entryBuilder.startBooleanToggle(Text.translatable("text.fpsdisplay.options.enabled"), config.enabled)
+        general.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("text.fpsdisplay.options.enabled"), config.enabled)
                 .setDefaultValue(configDefaults.enabled)
                 .setSaveConsumer(newValue -> config.enabled = newValue)
                 .build());
 
-        general.addEntry(entryBuilder.startBooleanToggle(Text.translatable("text.fpsdisplay.options.advancedStats"), config.advancedStats)
+        general.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("text.fpsdisplay.options.advancedStats"), config.advancedStats)
                 .setDefaultValue(configDefaults.advancedStats)
                 .setSaveConsumer(newValue -> config.advancedStats = newValue)
                 .build());
 
-        general.addEntry(entryBuilder.startFloatField(Text.translatable("text.fpsdisplay.options.textSize"), config.textSize)
+        general.addEntry(entryBuilder.startFloatField(new TranslatableText("text.fpsdisplay.options.textSize"), config.textSize)
                 .setDefaultValue(configDefaults.textSize)
                 .setSaveConsumer(newValue -> config.textSize = newValue)
                 .build());
-        general.addEntry(entryBuilder.startColorField(Text.translatable("text.fpsdisplay.options.textColor"), config.textColor)
+        general.addEntry(entryBuilder.startColorField(new TranslatableText("text.fpsdisplay.options.textColor"), config.textColor)
                 .setDefaultValue(configDefaults.textColor)
                 .setSaveConsumer(newValue -> config.textColor = newValue)
                 .build());
-        general.addEntry(entryBuilder.startIntSlider(Text.translatable("text.fpsdisplay.options.textAlpha"), config.textAlpha, 0, 255)
+        general.addEntry(entryBuilder.startIntSlider(new TranslatableText("text.fpsdisplay.options.textAlpha"), config.textAlpha, 0, 255)
                 .setDefaultValue(configDefaults.textAlpha)
                 .setSaveConsumer(newValue -> config.textAlpha = newValue)
                 .build());
-        general.addEntry(entryBuilder.startBooleanToggle(Text.translatable("text.fpsdisplay.options.textShadows"), config.textShadows)
+        general.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("text.fpsdisplay.options.textShadows"), config.textShadows)
                 .setDefaultValue(configDefaults.textShadows)
                 .setSaveConsumer(newValue -> config.textShadows = newValue)
                 .build());
 
-        general.addEntry(entryBuilder.startIntField(Text.translatable("text.fpsdisplay.options.offsetTop"), config.offsetTop)
+        general.addEntry(entryBuilder.startIntField(new TranslatableText("text.fpsdisplay.options.offsetTop"), config.offsetTop)
                 .setDefaultValue(configDefaults.offsetTop)
                 .setSaveConsumer(newValue -> config.offsetTop = newValue)
                 .build());
-        general.addEntry(entryBuilder.startIntField(Text.translatable("text.fpsdisplay.options.offsetLeft"), config.offsetLeft)
+        general.addEntry(entryBuilder.startIntField(new TranslatableText("text.fpsdisplay.options.offsetLeft"), config.offsetLeft)
                 .setDefaultValue(configDefaults.offsetLeft)
                 .setSaveConsumer(newValue -> config.offsetLeft = newValue)
                 .build());
 
-        general.addEntry(entryBuilder.startEnumSelector(Text.translatable("text.fpsdisplay.options.keybindMode"), ConfigData.KeyMode.class, config.keybindMode)
+        general.addEntry(entryBuilder.startEnumSelector(new TranslatableText("text.fpsdisplay.options.keybindMode"), ConfigData.KeyMode.class, config.keybindMode)
                 .setDefaultValue(configDefaults.keybindMode)
                 .setSaveConsumer(newValue -> config.keybindMode = newValue)
                 .build());
--- src/main/java/io/grayray75/mods/fpsdisplay/gui/FallbackOptionScreen.java
+++ src/main/java/io/grayray75/mods/fpsdisplay/gui/FallbackOptionScreen.java
@@ -3,17 +3,17 @@ package io.grayray75.mods.fpsdisplay.gui;
 import io.grayray75.mods.fpsdisplay.config.ConfigData;
 import io.grayray75.mods.fpsdisplay.config.ConfigManager;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ScreenTexts;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.screen.ScreenTexts;
-import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 
 public class FallbackOptionScreen extends Screen {
     protected final Screen parent;
     protected final ConfigData config;
 
     public FallbackOptionScreen(Screen parent) {
-        super(Text.translatable("text.fpsdisplay.options.fallback_title"));
+        super(new TranslatableText("text.fpsdisplay.options.fallback_title"));
         this.parent = parent;
         this.config = ConfigManager.getConfig();
     }
@@ -21,9 +21,9 @@ public class FallbackOptionScreen extends Screen {
     @Override
     protected void init() {
         // create done button
-        ButtonWidget doneButton = ButtonWidget.builder(ScreenTexts.DONE, (button) -> {
-            this.close();
-        }).width(200).position(this.width / 2 - 100, this.height - 27).build();
+        ButtonWidget doneButton = new ButtonWidget(this.width / 2 - 100, this.height - 27, 200, 20, ScreenTexts.DONE, (button) -> {
+            this.client.setScreen(this.parent);
+        });
         this.addDrawableChild(doneButton);
 
         super.init();
@@ -32,8 +32,8 @@ public class FallbackOptionScreen extends Screen {
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         this.renderBackground(matrices);
-        drawCenteredTextWithShadow(matrices, this.textRenderer, this.title, this.width / 2, 20, 0xFFFFFF);
-        drawCenteredTextWithShadow(matrices, this.textRenderer, Text.translatable("text.fpsdisplay.options.fallback_message"), this.width / 2, this.height / 2, 0xFFFFFF);
+        drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 20, 0xFFFFFF);
+        drawCenteredText(matrices, this.textRenderer, new TranslatableText("text.fpsdisplay.options.fallback_message"), this.width / 2, this.height / 2, 0xFFFFFF);
         super.render(matrices, mouseX, mouseY, delta);
     }
 

e11af830a1792884cadb9bbaca28b4241f73d5c5
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2023-09-27 15:35:07 +0200

    Add YACL settings screen

--- src/main/java/io/grayray75/mods/fpsdisplay/config/ModMenuIntegration.java
+++ src/main/java/io/grayray75/mods/fpsdisplay/config/ModMenuIntegration.java
@@ -4,12 +4,16 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import io.grayray75.mods.fpsdisplay.gui.ClothOptionScreen;
 import io.grayray75.mods.fpsdisplay.gui.FallbackOptionScreen;
+import io.grayray75.mods.fpsdisplay.gui.YaclOptionScreen;
 import net.fabricmc.loader.api.FabricLoader;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        if (FabricLoader.getInstance().isModLoaded("cloth-config")) {
+        if (FabricLoader.getInstance().isModLoaded("yet_another_config_lib_v3")) {
+            return parent -> YaclOptionScreen.generateScreen(parent);
+        }
+        else if (FabricLoader.getInstance().isModLoaded("cloth-config")) {
             return parent -> ClothOptionScreen.generateScreen(parent);
         }
         else {

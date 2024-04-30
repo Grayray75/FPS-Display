1690fe5c07b973b5ecc40c9158b4c56c0c65cb48
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2024-04-30 16:22:07 +0200

    Add unique annotation to renderText method

--- src/main/java/io/grayray75/mods/fpsdisplay/mixin/InGameHudMixin.java
+++ src/main/java/io/grayray75/mods/fpsdisplay/mixin/InGameHudMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -51,6 +52,7 @@ public class InGameHudMixin {
         }
     }
 
+    @Unique
     private void renderText(DrawContext context, TextRenderer textRenderer, String text, int x, int y, int color, float scale, boolean shadowed) {
         if (scale != 1.0f) {
             MatrixStack matrixStack = context.getMatrices();

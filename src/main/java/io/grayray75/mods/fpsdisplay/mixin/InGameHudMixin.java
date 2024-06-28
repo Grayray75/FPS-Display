c91e7910229ea3a3490291a337a92f4f77ce640f
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2024-06-28 22:00:22 +0200

    Update to Minecraft 1.21

--- src/main/java/io/grayray75/mods/fpsdisplay/mixin/InGameHudMixin.java
+++ src/main/java/io/grayray75/mods/fpsdisplay/mixin/InGameHudMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -17,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
     @Inject(at = @At("TAIL"), method = "render")
-    public void render(DrawContext context, float tickDelta, CallbackInfo info) {
+    public void render(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
         MinecraftClient client = MinecraftClient.getInstance();
         ConfigData config = ConfigManager.getConfig();
 

d8b45bb2d37ff0ba792cb3425dd147005d6f0356
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2025-04-17 10:56:27 +0200

    Save enabled state on client exit
    Fixes #21

--- src/main/java/io/grayray75/mods/fpsdisplay/FpsDisplayMod.java
+++ src/main/java/io/grayray75/mods/fpsdisplay/FpsDisplayMod.java
@@ -6,6 +6,7 @@ import io.grayray75.mods.fpsdisplay.mixin.MinecraftClientAccessor;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
@@ -45,6 +46,10 @@ public class FpsDisplayMod implements ClientModInitializer {
             else {
                 ShowOverlay = config.enabled;
             }
+        });
+
+        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> {
+            ConfigManager.saveConfig();
         });
     }
 }
